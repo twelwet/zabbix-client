8bdc9a18d6611a118e441ec45a71cff8872e1534
Maksim Akimov <maksimakimov@mbook14.local>
2022-12-30 17:18:16 +0300

    Small fix #2

--- src/xlsx-service/xlsx.js
+++ src/xlsx-service/xlsx.js
@@ -3,8 +3,10 @@ const XLSX = require('xlsx');
 const xlsx = {
 	newBook: () => XLSX.utils.book_new(),
 	jsonToSheet: (jsonData) => XLSX.utils.json_to_sheet(jsonData),
+	sheetToJson: (sheet) => XLSX.utils.sheet_to_json(sheet),
 	appendSheet: (book, sheet, sheetName) => XLSX.utils.book_append_sheet(book, sheet, sheetName),
 	getData: (book) => XLSX.write(book, { bookType: 'xlsx', type: 'buffer' }),
+	read: (path, opts) => XLSX.readFile(path, opts),
 };
 
 module.exports = xlsx;
