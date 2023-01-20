61d5e13d1e511d40d3f69cfaa04623ebe405f5e8
Maksim Akimov <akimovmax@mail.ru>
2023-01-20 16:50:01 +0300

    add some groups

--- src/cli-service/cli-config.js
+++ src/cli-service/cli-config.js
@@ -4,11 +4,10 @@ const CliConfig = {
 	// border dates for zabbix host groups
 	GROUPS_TO_DATES: {
 		[`${process.env.ZABBIX_HOST_GROUP_1}`]: new Date('2022-12-01T00:00:00'),
-		[`${process.env.ZABBIX_HOST_GROUP_2}`]: new Date('2022-09-01T00:00:00'),
+		[`${process.env.ZABBIX_HOST_GROUP_2}`]: new Date('2022-12-01T00:00:00'),
 		[`${process.env.ZABBIX_HOST_GROUP_3}`]: new Date('2022-12-01T00:00:00'),
-		[`${process.env.ZABBIX_HOST_GROUP_4}`]: new Date('2022-12-01T00:00:00'),
+		[`${process.env.ZABBIX_HOST_GROUP_4}`]: new Date('2022-01-01T00:00:00'),
 		[`${process.env.ZABBIX_HOST_GROUP_5}`]: new Date('2022-01-01T00:00:00'),
-		[`${process.env.ZABBIX_HOST_GROUP_6}`]: new Date('2022-01-01T00:00:00'),
 	},
 };
 
